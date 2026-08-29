2f0efa8f8adaaf3563684e64580f4df870d07326
Md Kawsar Ali <mdkawsarali346@gmail.com>
2026-08-29 21:29:19 +0600

    create getallpayment getmypayment and get single payment function

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,8 @@ import { UserRoutes } from "./app/module/user/user.route";
 import { getBkashIdToken } from "./app/lib/bkash";
 import { AppointementRoutes } from "./app/module/appointment/appointment.route";
 import { DoctorRoutes } from "./app/module/doctor/doctor.route";
+import { ScheduleRoutes } from "./app/module/schedule/schedule.route";
+import { PaymentRoutes } from "./app/module/payment/payment.route";
 
 const app: Application = express();
 
@@ -37,6 +39,8 @@ app.use("/api/v1/auth", AuthRoutes);
 app.use("/api/v1/user", UserRoutes);
 app.use("/api/v1/appointment", AppointementRoutes);
 app.use("/api/v1/doctors", DoctorRoutes);
+app.use("/api/v1/schedule", ScheduleRoutes);
+app.use("/api/v1/payment", PaymentRoutes);
 
 app.get("/test", async (req: Request, res: Response, next: NextFunction) => {
   try {
